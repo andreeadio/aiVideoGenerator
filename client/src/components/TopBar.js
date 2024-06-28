1325d72568524eacbba54e52ca9fccb04f586fbd
Andreea Dioclitean <diocliteanandreea21@stud.ase.ro>
2024-06-28 20:58:25 +0300

    added navigation links to the topbar

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Avatar } from 'primereact/avatar';
 import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Import Link
+
 import './TopBar.css';
 
 const TopBar = () => {
@@ -14,13 +16,36 @@ const TopBar = () => {
         // localStorage.removeItem('username'); // Remove the username on logout
         navigate('/login');
     };
+    const navigateToVideo = () => {
+        navigate('/video');
+    };
+
+    const navigateToGenerateImages = () => {
+        navigate('/generate-images');
+    };
 
     return (
         <div className="topbar">
-            <div>
+            {/* <div className="p-d-flex p-ai-center">
                 <img src="/logo.webp" alt="Logo" className="logo" />
+                <Link to="/generate-images" className="nav-link">Generate Video</Link>
+                <Link to="/video" className="nav-link">Video List</Link>
+
             </div>
+
             <div className="p-d-flex p-ai-center">
+                <Avatar icon="pi pi-user" className="p-mr-2" />
+                <span className="username">{username}</span>
+                <Button label=" Log out" icon="pi pi-sign-out" onClick={handleLogout} className="p-button-secondary" />
+            </div> */}
+
+
+            <div className="navigation-links">
+                <img src="/logo.webp" alt="Logo" className="logo" />
+                <Link to="/generate-images" className="nav-link">Generate Video</Link>
+                <Link to="/video" className="nav-link">Video List</Link>
+            </div>
+            <div className="user-section">
                 <Avatar icon="pi pi-user" className="p-mr-2" />
                 <span className="username">{username}</span>
                 <Button label=" Log out" icon="pi pi-sign-out" onClick={handleLogout} className="p-button-secondary" />
